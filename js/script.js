c9891fe58bae116871004152518b4fa52c47882d
Kartikancil96 <ara.rkhaidah@gmail.com>
2023-07-13 12:17:46 +0700

    update javascript

--- js/script.js
+++ js/script.js
@@ -4,3 +4,11 @@ const navbarNav = document.querySelector('.navbar-nav');
 document.querySelector('#hamburger').onclick = () => {
     navbarNav.classList.toggle('active');
 };
+
+const hamburGer = document.querySelector('#hamburger');
+
+document.addEventListener('click', function (e) {
+    if (!hamburGer.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove('active');
+    }
+});
